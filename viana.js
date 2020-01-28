154c0ba5d5e5826f6d5f24096f3d8a542a364df5
vianajunior <59456763+vianajunior@users.noreply.github.com>
2020-01-28 19:47:55 -0300

    Add files via upload

--- viana.js
+++ viana.js
@@ -1,3 +1,5 @@
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
@@ -23,4 +25,5 @@ byeSpeaker.speak = function(name) {
 // 'byeSpeaker' on the global scope as well.
 window.byeSpeaker = byeSpeaker;
 
-})(window);
+})(window)
+
--- viana2.js
+++ viana2.js
@@ -1,4 +1,5 @@
-
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
 
 (function(window) {
 	// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
